4744b3df1b0bd6e703445cfa883cb19ce57aae6b
Tina Yang <Tinayangers@gmail.com>
2023-03-06 11:02:22 -0500

    const navigate,location added

--- client/src/components/EditNote.jsx
+++ client/src/components/EditNote.jsx
@@ -3,6 +3,10 @@ import axios from 'axios'
 import { useState } from 'react'
 
 const EditNote = () => {
+    let navigate = useNavigate()
+    const location = useLocation()
+    const { editNote } = location.state
+    console.log(editNote)
 
     return (
         <div>EditNote</div>
