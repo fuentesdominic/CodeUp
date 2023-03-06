4051df224efe1561c1ffe316aadfe105d27f7a08
Dominic Fuentes <militaryforce1@yahoo.com>
2023-03-06 12:58:56 -0600

    add map to task.js and links in home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,33 +1,35 @@
-import { useState, useEffect } from "react"
+import '../App.css'
 import { Link } from 'react-router-dom'
 import axios from "axios"
 
 const Home = () => {
 
-  const [allCategories, setAllCategories] = useState([])
-
-  const getAllCategories = async () => {
-    try {
-      let res = await axios.get(`/tasks/`)
-      setAllCategories(res.data)
-      // console.log(res.data)
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  useEffect(() => {
-    getAllCategories()
-  }, [])
-
   return (
-    <div>
-      {allCategories.map((category) => (
-          <Link to={`/${allCategories.allCategories}`} state={{task:category}}>
-            <h2 className="homecategories">{category.category}</h2>
+    <div className='homepageicons'>
+        <div>
+        <h2 className='homepagetext'>HTML</h2>
+          <Link to={''}>
+            <img className="homepageicons" src="https://cdn-icons-png.flaticon.com/512/732/732212.png" alt=""></img>
           </Link>
-       
-      ))}
+        </div>
+        <div>
+          <h2 className='homepagetext'>Javascript</h2>
+          <Link to={''}>
+            <img className="homepageicons" src="https://logospng.org/download/javascript/logo-javascript-icon-1024.png" alt=""></img>
+          </Link>
+        </div>
+        <div>
+          <h2 className='homepagetext'>CSS</h2>
+          <Link to={''}>
+            <img className="homepageicons" src="https://cdn-icons-png.flaticon.com/512/732/732190.png" alt=""></img>
+          </Link>
+        </div>
+        <div>
+          <h2 className='homepagetext'>React</h2>
+          <Link to={''}>
+            <img className="homepageicons" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" alt=""></img>
+          </Link>
+        </div>
     </div>
   )
 }
