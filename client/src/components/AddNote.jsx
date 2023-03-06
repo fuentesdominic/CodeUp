c181d60d3f6380042b965a51474b993768e774b6
Tina Yang <Tinayangers@gmail.com>
2023-03-06 12:36:15 -0500

    Added const taskId, useState-newNote, and navigate

--- client/src/components/AddNote.jsx
+++ client/src/components/AddNote.jsx
@@ -3,7 +3,9 @@ import axios from 'axios'
 import { useState } from 'react'
 
 const AddNote = () => {
-
+    let navigate = useNavigate()
+    const { taskId } = useParams()
+    const [newNote, setNewNote] = useState({ notes: '' })
     return (
         <div>AddNote</div>
     )
