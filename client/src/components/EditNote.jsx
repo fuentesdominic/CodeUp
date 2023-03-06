33445204ad909aa1ef72dd6bbc994a5dff0f0ce3
Tina Yang <Tinayangers@gmail.com>
2023-03-06 10:56:24 -0500

    Added import location/navigate/params/axios/state

--- client/src/components/EditNote.jsx
+++ client/src/components/EditNote.jsx
@@ -1,4 +1,9 @@
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import axios from 'axios'
+import { useState } from 'react'
+
 const EditNote = () => {
+
     return (
         <div>EditNote</div>
     )
