21f4ec171c03aee9ff11bd7ac8eb2784120b0f84
Tina Yang <Tinayangers@gmail.com>
2023-03-06 12:33:58 -0500

    Added import navigate/params/axios/state to addnote

--- client/src/components/AddNote.jsx
+++ client/src/components/AddNote.jsx
@@ -1,4 +1,9 @@
+import { useNavigate, useParams } from 'react-router-dom'
+import axios from 'axios'
+import { useState } from 'react'
+
 const AddNote = () => {
+
     return (
         <div>AddNote</div>
     )
