206392dbd0fb5cc059f4f7a50e1b75230fefc227
Dominic Fuentes <militaryforce1@yahoo.com>
2023-03-06 18:29:30 -0600

    complete routes for categories

--- client/src/pages/Category.js
+++ client/src/pages/Category.js
@@ -1,6 +1,36 @@
-const Category = () => {
+import { useState, useEffect } from "react"
+import { Link } from 'react-router-dom'
+import axios from "axios"
+import { useParams } from "react-router-dom"
+
+const Task = () => {
+
+  const {category} = useParams()
+  const [allTasks, setAllTasks] = useState([])
+
+  const getAllTasks = async () => {
+    try {
+      let res = await axios.get(`/tasks/${category}`)
+      setAllTasks(res.data)
+      // console.log(res.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getAllTasks()
+  }, [])
+
   return (
-    <div>Category</div>
+    <div>
+      {allTasks.map((task) => (
+          <Link to={`/task/${task.id}`}>
+            <h2>{task.title}</h2>
+          </Link>
+       
+      ))}
+    </div>
   )
 }
-export default Category
+export default Task
