bec4529df0b071a2f09a68dae82158d4763da1da
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-03-07 12:59:28 -0600

    changed Home routes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,35 +1,50 @@
-import '../App.css'
-import { Link } from 'react-router-dom'
+import "../App.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-
   return (
-    <div className='homepageicons'>
-        <div>
-        <h2 className='homepagetext'>HTML</h2>
-          <Link to={`/html`}>
-            <img className="homepageicons" src="https://cdn-icons-png.flaticon.com/512/732/732212.png" alt=""></img>
-          </Link>
-        </div>
-        <div>
-          <h2 className='homepagetext'>Javascript</h2>
-          <Link to={`/javascript`}>
-            <img className="homepageicons" src="https://logospng.org/download/javascript/logo-javascript-icon-1024.png" alt=""></img>
-          </Link>
-        </div>
-        <div>
-          <h2 className='homepagetext'>CSS</h2>
-          <Link to={`/css`}>
-            <img className="homepageicons" src="https://cdn-icons-png.flaticon.com/512/732/732190.png" alt=""></img>
-          </Link>
-        </div>
-        <div>
-          <h2 className='homepagetext'>React</h2>
-          <Link to={`/react`}>
-            <img className="homepageicons" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" alt=""></img>
-          </Link>
-        </div>
+    <div className="homepageicons">
+      <div>
+        <h2 className="homepagetext">HTML</h2>
+        <Link to={`/task/html`}>
+          <img
+            className="homepageicons"
+            src="https://cdn-icons-png.flaticon.com/512/732/732212.png"
+            alt=""
+          ></img>
+        </Link>
+      </div>
+      <div>
+        <h2 className="homepagetext">Javascript</h2>
+        <Link to={`/task/javascript`}>
+          <img
+            className="homepageicons"
+            src="https://logospng.org/download/javascript/logo-javascript-icon-1024.png"
+            alt=""
+          ></img>
+        </Link>
+      </div>
+      <div>
+        <h2 className="homepagetext">CSS</h2>
+        <Link to={`/task/css`}>
+          <img
+            className="homepageicons"
+            src="https://cdn-icons-png.flaticon.com/512/732/732190.png"
+            alt=""
+          ></img>
+        </Link>
+      </div>
+      <div>
+        <h2 className="homepagetext">React</h2>
+        <Link to={`/task/react`}>
+          <img
+            className="homepageicons"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+            alt=""
+          ></img>
+        </Link>
+      </div>
     </div>
-  )
-}
-export default Home
+  );
+};
+export default Home;
