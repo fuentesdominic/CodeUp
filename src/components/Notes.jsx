b0488052b0f4ac5daa7c70e6a6affd9370870dbc
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-03-08 07:26:17 -0600

    added conditional rendering to the add notes button

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,62 +1,64 @@
-import { useState, useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import { FaTrashAlt } from 'react-icons/fa'
-import { GetNotesById } from '../services/TaskServices'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { FaTrashAlt } from "react-icons/fa";
+import { GetNotesById } from "../services/TaskServices";
+import axios from "axios";
 
 const Notes = () => {
-  const [allNotes, setAllNotes] = useState([])
-  const { userId } = useParams()
-  const { taskId } = useParams()
-  console.log(taskId)
-  console.log(userId)
-
+  const [allNotes, setAllNotes] = useState([]);
+  const { userId } = useParams();
+  const { taskId } = useParams();
+  console.log(taskId);
+  console.log(userId);
 
   const getNotes = async (e) => {
     try {
-      const res = await GetNotesById(taskId, userId)
-      setAllNotes(res)
-      console.log(res)
+      const res = await GetNotesById(taskId, userId);
+      setAllNotes(res);
+      console.log(res);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   useEffect(() => {
-    getNotes()
-  }, [])
+    getNotes();
+  }, []);
 
   const handleDelete = async (id) => {
-    await axios.delete(`/`)
-    getNotes()
-  }
+    await axios.delete(`/`);
+    getNotes();
+  };
 
   return (
-    <div className='notes'>
-      <h1 className='noteTitle'>Notes</h1>
+    <div className="notes">
+      <h1 className="noteTitle">Notes</h1>
       <div className="notesContainer">
         {allNotes.map((note) => (
           <div className="note">
-            <Link to={`/task/${taskId}/user/${userId}/addnote`}
-            >
-              <button className='addNote'>+ Add Note</button>
+            {/* {allNotes.length > 0 ? null : ( */}
+            <Link to={`/task/${taskId}/user/${userId}/addnote`}>
+              <button className="addNote">+ Add Note</button>
             </Link>
+            {/* )} */}
             <h1>{note.notes}</h1>
-            <Link to={`/task/${taskId}/user/${userId}/editnote`} state={{ origNote: note }}>
+            <Link
+              to={`/task/${taskId}/user/${userId}/editnote`}
+              state={{ origNote: note }}
+            >
               <button>Edit Note</button>
             </Link>
             <FaTrashAlt
               onClick={() => handleDelete()}
-              role='button'
-              tabIndex='0'
+              role="button"
+              tabIndex="0"
               aria-label={`Delete ${note.id}`}
-              className='delete'
+              className="delete"
             />
           </div>
-        )
-        )}
+        ))}
       </div>
     </div>
-  )
-}
-export default Notes
+  );
+};
+export default Notes;
