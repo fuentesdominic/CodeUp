4e533c184b3b81181e4a18a68ee361ab08ce51dc
notryancho <ryancho1226@hotmail.com>
2023-03-09 00:03:56 -0500

    navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,40 +2,50 @@ import { Link } from 'react-router-dom';
 
 
 const Nav = ({ user, handleLogOut }) => {
-  let userOptions
+  let userOptions;
   if (user) {
     userOptions = (
       <nav className="nav-links">
-        <h3 className="welcome-message">Welcome {user.email}!</h3>
-        <Link className="nav-link" to="/">Home</Link>
-        <Link className="nav-link" to="/about">About</Link>
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+        <Link className="nav-link" to="/about">
+          About
+        </Link>
         <Link className="nav-link" onClick={handleLogOut} to="/signin">
           Sign Out
         </Link>
       </nav>
-    )
+    );
   }
 
   const publicOptions = (
     <nav className="nav-links">
-      <Link className="nav-link" to="/register">Register</Link>
-      <Link className="nav-link" to="/signin">Sign In</Link>
-      <Link className="nav-link" to="/about">About</Link>
+      <Link className="nav-link" to="/register">
+        Register
+      </Link>
+      <Link className="nav-link" to="/signin">
+        Sign In
+      </Link>
+      <Link className="nav-link" to="/about">
+        About
+      </Link>
     </nav>
-  )
+  );
 
   return (
     <header>
       <Link to="/">
-        <div className="logo-wrapper" alt="logo">
-          
-        </div>
+        <div className="logo-wrapper" alt="logo"></div>
       </Link>
+      <div className="welcome-message-container">
+        {user && <h3 className="welcome-message">Welcome {user.email}!</h3>}
+      </div>
       <div className="nav-container">
         {user ? userOptions : publicOptions}
       </div>
     </header>
-  )
-}
+  );
+};
 
 export default Nav;
